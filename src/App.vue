a3c85bb15bf48d639fef7f61fd51eaa6e6eddc4f
Daniele206 <danieledeieso2004@gmail.com>
2024-05-31 17:34:10 +0200

    [add] paginetor

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
   import axios from 'axios';
   //axios
 
+  // store
+  import { store } from './data/store';
+  // store
+
   import ProjectCard from './components/ProjectCard.vue';
 
   export default {
@@ -13,22 +17,39 @@
     
     data(){
       return{
+        store,
         projects: [],
+        page: '',
+        totPage: '',
         redyReed: false
       }
     },
 
     methods:{
       getApi(){
-        axios.get('http://127.0.0.1:8000/api/projects')
+        axios.get(this.store.apiUrl, {
+          params: this.store.params
+        })
           .then(result => {
             this.redyReed = true;
+            this.page = result.data.current_page
+            this.totPage = result.data.last_page
             this.projects = result.data.data;
             console.log(this.projects);
           })
           .catch(error => {
             console.log(error);
           })
+      },
+
+      pageswap(isNext){
+        if(isNext){
+          this.store.params.page++;
+        }else{
+          this.store.params.page--;
+        }
+        
+        this.getApi()
       }
     },
 
@@ -54,6 +75,13 @@
         </div>
       </div>
       <h4 v-else>Loading...</h4>
+      <nav v-if="redyReed" aria-label="Page navigation example">
+        <ul class="pagination">
+          <li v-if="page > 1" class="page-item"><button @click="pageswap(false)" class="page-link"><i class="fa-solid fa-caret-left"></i></button></li>
+          <li class="page-item"><p class="page-link">Pgina {{ page }} di {{ totPage }}</p></li>
+          <li v-if="page < totPage" class="page-item"><button @click="pageswap(true)" class="page-link"><i class="fa-solid fa-caret-right"></i></button></li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
