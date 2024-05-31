97c57491cb2249dc1314b87e53c8811f3e92cfc2
Daniele206 <danieledeieso2004@gmail.com>
2024-05-31 17:12:47 +0200

    [add] milestone 5

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,18 @@
   import axios from 'axios';
   //axios
 
+  import ProjectCard from './components/ProjectCard.vue';
+
   export default {
+
+    components:{
+      ProjectCard
+    },
     
     data(){
       return{
-        projects: []
+        projects: [],
+        redyReed: false
       }
     },
 
@@ -15,6 +22,7 @@
       getApi(){
         axios.get('http://127.0.0.1:8000/api/projects')
           .then(result => {
+            this.redyReed = true;
             this.projects = result.data.data;
             console.log(this.projects);
           })
@@ -35,17 +43,22 @@
 <template>
   <div class="main_container">
     <div>
-      <h1 class="fw-bold mt-4">Lista Progetti</h1>
-      <ul>
-        <li v-for="(project, i) in projects" :key="i">{{ project.name }}</li>
-      </ul>
+      <h1 class="fw-bold mt-4 text-center">Lista Progetti</h1>
+      <div class="container" v-if="redyReed">
+        <div class="row justify-content-center">
+          <ProjectCard
+          v-for="(project, i) in projects"
+          :key="i"
+          :project="project"
+          />
+        </div>
+      </div>
+      <h4 v-else>Loading...</h4>
     </div>
   </div>
 </template>
 
 
 <style lang="scss">
-
-  @use './assets/scss/main.scss';
 
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,28 @@
+<script>
+  export default {
+    props:{
+      project: Object
+    }
+  }
+</script>
+
+<template>
+  <div class="card mx-3 my-4" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title text-primary fw-bold">{{ project.name }}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.type.name }}</h6>
+    <h6 class="text-primary fw-bold">Descrizzione</h6>
+    <p class="card-text d-block overflow-auto" style="height: 200px;">{{ project.description }}</p>
+    <div v-if="project.technologies.length > 0">
+      <h6 class="text-primary fw-bold">Tecnologie</h6>
+      <ul>
+        <li v-for="technology in project.technologies" :key="technology.id">{{ technology.name }}</li>
+      </ul>
+    </div>
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
